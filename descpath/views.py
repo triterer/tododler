daf9bc40b21242e311ddf725d2bec306534fcb2a
triterer <vofka99@rambler.ru>
2019-12-07 20:10:31 +0300

    rmsht

--- descpath/views.py
+++ descpath/views.py
@@ -7,18 +7,7 @@ import json
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from .models import Messages
-#def index(request):
-#    return HttpResponse("Hello, world. You're at the polls index.")
 
-#def detail(request, question_id):
-#    return HttpResponse("You're looking at question %s." % question_id)
-
-#def results(request, question_id):
- #   response = "You're looking at the results of question %s."
-##    return HttpResponse(response % question_id)
-
-#def vote(request, question_id):
- #   return HttpResponse("You're voting on question %s." % question_id)
 @csrf_exempt
 def registration(request):
     recieved = json.loads(request.body)
